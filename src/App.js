1ea5b334be5b72132dc0ae7b0ffeb355a7099389
AKen00 <ajken04@gmail.com>
2021-01-03 18:47:52 -0500

    gitignore

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import About from './components/About';
+
+function App() {
+  return (
+    <main>
+    <About></About>
+  </main>
+  );
+}
+
+export default App;
