374640a49d850d9a00785c01dda547867958ab3e
AKen00 <ajken04@gmail.com>
2021-01-04 21:39:48 -0500

    updated files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+// import './App.css';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Nav from './components/Nav';
 import About from './components/About';
 
 function App() {
   return (
-    <main>
-    <About></About>
-  </main>
+    <div>
+      <Nav></Nav>
+      <main>
+        <About></About>
+      </main>
+    </div>
   );
 }
 
